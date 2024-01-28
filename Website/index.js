1de5423d92c89f7813f202e18509015dbe382fed
Milesh <152176996+Milesh01@users.noreply.github.com>
2024-01-28 22:58:03 +0530

    Update index.js

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,8 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+document.addEventListener('DOMContentLoaded', function () {
+  const navBar = document.querySelector('.nav-bar');
+  const toggleBtn = document.getElementById('toggle-btn');
+
+  toggleBtn.addEventListener('click', function () {
+    navBar.classList.toggle('show');
+  });
+});
