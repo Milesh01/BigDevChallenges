6d81bdf9fc24dfd4b329d886cb7773d7d8c7fe74
Milesh <152176996+Milesh01@users.noreply.github.com>
2024-03-18 00:10:06 +0530

    oNbOARDING JS

--- Any Chat/index.js
+++ Any Chat/index.js
@@ -1,6 +1,7 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+const colmd6 = document.querySelector('.col-md-6');
+const imgId = document.getElementById('imgId');
+document.getElementById('startBtn').addEventListener('click',()=>{
+  colmd6.style.opacity = '0';
+  imgId.style.opacity = '0';
+  window.location.href = "chat.html";
+})
